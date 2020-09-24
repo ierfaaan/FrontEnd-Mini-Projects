b5730db024522c8035876bf9295260a200db88f3
Erf <erfan.ab9898@gmail.com>
2020-09-24 17:41:13 +0330

    The main template of the website is designed

---
+++ mian-Js/main.js
@@ -0,0 +1,10 @@
+// const btnScroll = document.querySelector(".header__btn--scroll")
+// const container = document.querySelector(".container")
+// btnScroll.addEventListener("click",scroll)
+
+// function scroll(){
+//   container.style.scrollSnapType = "none"
+//   let h = container.firstElementChild.clientHeight
+//   container.scrollTo(0,635);
+//   container.style.scrollSnapType = " y mandatory"
+// }
